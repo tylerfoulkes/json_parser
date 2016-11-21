e9be83e724779584d8fb953e9a5947b1d2817701
Tyler Foulkes <tylerfoulkes@Tyler-Foulkess-MacBook.local>
2016-11-20 21:20:52 -0700

    Added more comments

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,19 +12,22 @@ router.get('/', function(req, res, next) {
 
 	/* 
 		* Parse through JSON object and calculate total prices for all watches,
-	   	* Clock, and all products
+	   	* clocks, and all products
 	*/
 	for(var k = 0; k < dataFile.length; k++) {
 		for(var i = 0; i < dataFile[k].products.length; i++) {
 	    	for(var j = 0; j < dataFile[k].products[i].variants.length; j++) {
+	    		// If product type is clock increment clock total and total price
 	    		if(dataFile[k].products[i].product_type == 'Clock') {
 	    			clock_total = Number(dataFile[k].products[i].variants[j].price) + clock_total;
 	    			total_price = Number(dataFile[k].products[i].variants[j].price) + total_price;
 	    		}
+	    		// If product type is watch increment watch total and total price
 	    		else if(dataFile[k].products[i].product_type == 'Watch') {
 	    			watch_total = Number(dataFile[k].products[i].variants[j].price) + watch_total;
 	    			total_price = Number(dataFile[k].products[i].variants[j].price) + total_price;
 	    		}
+	    		// Else just increment product total price
 	    		else {
 	    			total_price = Number(dataFile[k].products[i].variants[j].price) + total_price;
 	    		}
@@ -32,6 +35,7 @@ router.get('/', function(req, res, next) {
 		}
 	}
 
+	// Round each total to nearest two decmial places
 	clock_total = Math.round(clock_total * 100) / 100;
 	watch_total = Math.round(watch_total * 100) / 100;
 	total_price = Math.round(total_price * 100) / 100;
